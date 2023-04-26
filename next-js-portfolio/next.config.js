347c53dc5bf280c420b103f72a29911d32b679fa
kin-cunico <124842064+kin-cunico@users.noreply.github.com>
2023-04-26 15:38:46 +0100

    010

--- next-js-portfolio/next.config.js
+++ next-js-portfolio/next.config.js
@@ -6,10 +6,7 @@ const nextConfig = {
 module.exports = nextConfig;
 
 module.exports = {
-	// https://github.com/vercel/next.js/issues/21079
-	// Remove this workaround whenever the issue is fixed
 	images: {
-		loader: "imgix",
-		path: "/",
+		unoptimized: true,
 	},
 };
